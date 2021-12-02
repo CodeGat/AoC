9b9d5a858b611c1032dfecc64ba4812d3273447c
CodeGat <tommy.gatti@outlook.com>
2021-12-02 18:04:21 +1100

    Added solutions for day 2

---
+++ 2021/d2p2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char **argv){
+    ifstream in (argv[1]);
+    string direction;
+    string amount;
+    int horizontal = 0;
+    int vertical = 0;
+    int aim = 0;
+
+    while (in >> direction >> amount){
+        if (direction == "forward"){
+            horizontal += stoi(amount);
+            vertical += aim * stoi(amount);
+        } else if (direction == "up"){
+            aim -= stoi(amount);
+        } else if (direction == "down"){
+            aim += stoi(amount);
+        } else {
+            cout << "Something happened" << endl;
+        }
+    }
+
+    printf("Result: %d\n", horizontal * vertical);
+    return 0;
+}
