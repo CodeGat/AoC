2a7caf16ef2ab7a99f13be097158a3ec717674a6
Tommy Gatti <tommy.gatti@anu.edu.au>
2023-12-09 16:35:09 +1100

    Complete d7p2

--- 2023/d7/p2.py
+++ 2023/d7/p2.py
@@ -3,48 +3,56 @@ from operator import indexOf
 
 def ranking(hand: tuple[str, int]) -> list[int]:
     result: dict[str, int] = {}
-    
+
     for card in hand[0]:
         if card in result:
             result[card] += 1
         else:
             result[card] = 1
-            
+
     count: list[int] = sorted(list(result.values()), reverse=True)
-    jokers: int = len([c for c in hand[0] if c == 'J'])
+
+    jokers: int = result.pop('J', 0)
+
+    j_removed_count: list[int] = sorted(list(result.values()), reverse=True)
+    joker_count: list[int] = list(j_removed_count) if len(j_removed_count) != 0 else [0]
+
     for _ in range(jokers):
         # add one to the highest
-        count[0] += 1
-        # subtract one from the lowest
-        if count[-1] == 1:
-            count = count[:-1]
-        else:
-            count[-1] -= 1
-    
-    return sorted(count, reverse=True)
+        joker_count[0] += 1
+        # # subtract one from the lowest
+        # if joker_count[-1] == 1:
+        #     joker_count = joker_count[:-1]
+        # else:
+        #     joker_count[-1] -= 1
+
+    # if jokers > 0:
+    print(f'{hand[0]} w {jokers} J: {count}->{j_removed_count} + {jokers}J->{joker_count}')
+
+    return sorted(joker_count, reverse=True)
 
 def compare_hands(h1: tuple[str, int], h2: tuple[str, int]) -> int:
     ranking: str = "J23456789TQKA"
-    
+
     for i in range(len(h1[0])):
         i1: int = ranking.index(h1[0][i])
         i2: int = ranking.index(h2[0][i])
-        
+
         if i1 < i2:
             return 1
         elif i1 > i2:
             return -1
-    
+
     return 0
 
 with open('./2023/d7/in.txt') as f:
     hands_components: list[list[str]] = [line.split() for line in f]
     hands: list[tuple[str, int]] = [(h[0], int(h[1])) for h in hands_components]
-    
+
     hand_types: list[list[tuple[str, int]]] = [[] for _ in range(7)]
-        
+
     for hand in hands:
-        rank: list[int] = ranking(hand)  
+        rank: list[int] = ranking(hand)
         match rank:
             case [5]:
                 hand_types[0].append(hand)
@@ -60,16 +68,18 @@ with open('./2023/d7/in.txt') as f:
                 hand_types[5].append(hand)
             case _:
                 hand_types[6].append(hand)
-    
+
     hand_strengths: list[list[tuple[str, int]]] = [sorted(hand, key=cmp_to_key(compare_hands)) for hand in hand_types]
-    
+
     print(hand_strengths)
-    print('-' * 10)
-    
+    print('\n')
+
     ordering_by_strength: list[tuple[str, int]] = reduce(lambda l1, l2: l1 + l2, hand_strengths)
-    
     ordering_by_lowest_rank: list[tuple[str, int]] = list(reversed(ordering_by_strength))
+
+    print(ordering_by_lowest_rank)
+
     winnings: list[int] = [bid * (rank + 1) for rank, (_, bid) in enumerate(ordering_by_lowest_rank)]
-    
+
     print(sum(winnings))
-    
+
